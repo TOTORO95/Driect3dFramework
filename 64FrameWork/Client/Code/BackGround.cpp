861659a2fc98136f813030e7e05b4bd4765ffe6d
cat57 <cat5778@naver.com>
2020-05-29 04:58:11 +0900

    MapInit

---
+++ 64FrameWork/Client/Code/BackGround.cpp
@@ -0,0 +1,94 @@
+#include "stdafx.h"
+#include "BackGround.h"
+
+#include "Export_Function.h"
+
+CBackGround::CBackGround(LPDIRECT3DDEVICE9 pGraphicDev)
+	: Engine::CGameObject(pGraphicDev)
+{
+
+}
+
+CBackGround::~CBackGround(void)
+{
+
+}
+
+HRESULT CBackGround::Ready_GameObject(void)
+{
+	FAILED_CHECK_RETURN(Add_Component(), E_FAIL);
+
+	m_pTransformCom->m_vScale.x = m_pTextureCom->Get_ImageInfo().Width;
+	m_pTransformCom->m_vScale.y = m_pTextureCom->Get_ImageInfo().Height;
+	m_pTransformCom->m_vScale.z = 0.5f;
+	m_pTransformCom->m_vInfo[Engine::INFO_POS] = _vec3{ 0,20,0.01f };
+
+	
+	return S_OK;
+}
+
+_int CBackGround::Update_GameObject(const _float& fTimeDelta)
+{
+	m_fFrameCnt += m_fFrameMax * fTimeDelta * 0.5f;
+
+	if (m_fFrameCnt >= m_fFrameMax)
+		m_fFrameCnt = 0.f;
+
+
+	Engine::CGameObject::Update_GameObject(fTimeDelta);	
+	m_pRendererCom->Add_RenderGroup(Engine::RENDER_UI, this);
+
+
+	return 0;
+}
+
+void CBackGround::Render_GameObject(void)
+{
+	m_pGraphicDev->SetTransform(D3DTS_VIEW, &m_pTransformCom->m_matWorld);
+	m_pTextureCom->Render_Texture((_uint)m_fFrameCnt);
+	m_pBufferCom->Render_Buffer();
+}
+
+HRESULT CBackGround::Add_Component(void)
+{
+	Engine::CComponent*		pComponent = nullptr;
+
+	pComponent = m_pBufferCom = dynamic_cast<Engine::CRcTex*>(Engine::Clone(RESOURCE_STATIC, L"Buffer_RcTex"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_pComponentMap[Engine::ID_STATIC].emplace(L"Com_Buffer", pComponent);
+
+	pComponent = m_pTransformCom = Engine::CTransform::Create();
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_pComponentMap[Engine::ID_DYNAMIC].emplace(L"Com_Transform", pComponent);
+
+	pComponent = m_pTextureCom = dynamic_cast<Engine::CTexture*>(Engine::Clone(RESOURCE_LOGO, L"Texture_Logo"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_pComponentMap[Engine::ID_STATIC].emplace(L"Com_Texture", pComponent);
+
+	pComponent = m_pRendererCom = Engine::Get_Renderer();
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	pComponent->AddRef();
+	m_pComponentMap[Engine::ID_STATIC].emplace(L"Com_Renderer", pComponent);
+	
+	return S_OK;
+}
+
+
+
+CBackGround* CBackGround::Create(LPDIRECT3DDEVICE9 pGraphicDev)
+{
+	CBackGround*	pInstance = new CBackGround(pGraphicDev);
+
+	if (FAILED(pInstance->Ready_GameObject()))
+		Engine::Safe_Release(pInstance);
+
+	return pInstance;
+}
+
+void CBackGround::Free(void)
+{
+
+
+	Engine::CGameObject::Free();
+}
+
---
+++ 64FrameWork/Engine/Utility/Code/Camera.cpp
@@ -0,0 +1,45 @@
+
+#include "Camera.h"
+#include "Pipeline.h"
+
+USING(Engine)
+
+Engine::CCamera::CCamera(LPDIRECT3DDEVICE9 pGraphicDev)
+	: CGameObject(pGraphicDev)
+{
+
+}
+
+Engine::CCamera::~CCamera(void)
+{
+
+}
+
+HRESULT Engine::CCamera::Ready_GameObject(void)
+{
+	D3DXMatrixLookAtLH(&m_matView, &m_vEye, &m_vAt, &m_vUp);
+	//CPipeline::MakeViewSpaceMatrix(&m_matView, &m_vEye, &m_vAt, &m_vUp);
+	m_pGraphicDev->SetTransform(D3DTS_VIEW, &m_matView);
+
+	D3DXMatrixPerspectiveFovLH(&m_matProj, m_fFovY, m_fAspect, m_fNear, m_fFar);
+	//CPipeline::MakeProjectionMatrix(&m_matProj, m_fFovY, m_fAspect, m_fNear, m_fFar);
+	m_pGraphicDev->SetTransform(D3DTS_PROJECTION, &m_matProj);
+
+	return S_OK;
+}
+
+_int Engine::CCamera::Update_GameObject(const _float& fTimeDelta)
+{
+	D3DXMatrixLookAtLH(&m_matView, &m_vEye, &m_vAt, &m_vUp);
+	//CPipeline::MakeViewSpaceMatrix(&m_matView, &m_vEye, &m_vAt, &m_vUp);
+	m_pGraphicDev->SetTransform(D3DTS_VIEW, &m_matView);
+
+	return 0;
+}
+
+void Engine::CCamera::Free(void)
+{
+	CGameObject::Free();
+}
+
+
