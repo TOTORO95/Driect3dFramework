8e48cab294548cb5ae711745a164d2d5a483ffe5
cat57 <cat57@DESKTOP-2B3FUK7>
2020-05-29 05:03:45 +0900

    Init64

--- FrameWark/CopyResources.bat
+++
@@ -1,7 +0,0 @@
-copy		.\Engine\Resources\Code\*.h		.\Reference\Header\		
-copy		.\Engine\Resources\Bin\Resources.dll	.\Client\Bin\		
-copy		.\Engine\Resources\Bin\Resources.dll	.\Tool\Bin\		
-copy		.\Engine\Resources\Bin\Resources.lib	.\Reference\Library\	
-
-copy		.\Engine\Header\*.h			.\Reference\Header\
-copy		.\Engine\Header\*.inl			.\Reference\Header\
